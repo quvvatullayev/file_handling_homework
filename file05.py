61f8ccc542790853bfd9ef53c98c427f7a2e2912
tajriba <quvvatullayev@gmail.com>
2022-07-06 17:35:42 +0300

    file05.py

--- file05.py
+++ file05.py
@@ -6,5 +6,23 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    list1 = []
+    son = 0
+    harf = 0
+    i = 0
+    while i < len(data):
+        if data[i].isalpha():
+            harf += 1
+        elif data[i].isdigit():
+            son += 1
+
+        i += 1
+    list1.append(son)
+    list1.append(harf)
+
+
+    return list1
     
 # Read data from file
+f = open("txt_file/data05.txt").read()
+print(main(f))
