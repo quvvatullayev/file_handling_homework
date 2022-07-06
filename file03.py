482e62c5898d43ccb32aefaa53d3a3fb81a295f7
tajriba <quvvatullayev@gmail.com>
2022-07-06 17:21:01 +0300

    file03.py

--- file03.py
+++ file03.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    x = []
+    i = 0
+    while i < len(data):
+        if data[i].isdigit():
+            x.append(i)
+        i += 1
+    return x
     
 # Read data from file
+f = open("txt_file/data03.txt").read()
+print(main(f))
