95eda8a7a5cab541011a02f8d72b1b4db1b6ff05
tajriba <quvvatullayev@gmail.com>
2022-07-06 17:10:18 +0300

    file02.py

--- file02.py
+++ file02.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    sum = 0
+    for i in data:
+        sum += 1
+    return sum
 
 # Read data from file
+f = open("txt_file/data02.txt").read()
+print(main(f))
