a42fd9cb0518c9be684d5a165c9a3246d2117af0
tajriba <quvvatullayev@gmail.com>
2022-07-06 19:03:37 +0300

    file06.py

--- file06.py
+++ file06.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    list1 = data.split()
+    list2 = []
+    for i in list1:
+        list2.append(len(i))
+    return list2
+
 # Read data from file
+f = open("txt_file/data06.txt").read()
+print(main(f))
