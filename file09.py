b2351f4f0c8990b965cce7786b425cbee6466e65
tajriba <quvvatullayev@gmail.com>
2022-07-06 19:19:39 +0300

    file09.py

--- file09.py
+++ file09.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    min_list = []
+    i = 0
+    while i < len(data):
+        if data[i].isdigit():
+            min_list.append(int(data[i]))
+        i += 1
+    return min(min_list)
 
 # Read data from file
+f = open("txt_file/data09.txt").read()
+print(main(f))
