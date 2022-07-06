c620de537546dc5b329059a76df06143ecec24ab
tajriba <quvvatullayev@gmail.com>
2022-07-06 19:08:37 +0300

    file07.py

--- file07.py
+++ file07.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    sum = 0
+    i = 0
+    while i < len(data):
+        if data[i].isdigit():
+            sum += int(data[i])
+        i += 1
+
+    return sum
+
 # Read data from file
+f = open("txt_file/data07.txt").read()
+print(main(f))
